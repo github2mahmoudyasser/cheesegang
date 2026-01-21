d238b5ef6af87f5491dc124795a77b525386071d
Mahmoud Yasser <mahmoudyaser20196@gmail.com>
2026-01-21 17:49:41 +0200

    add adapter to main file

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/navigation/app_navigator.dart';
 import 'core/network/api_service.dart';
 import 'features/auth/data/auth_repo.dart';
+import 'features/cart/data/cart_model.dart';
 import 'features/home/data/models/product_model.dart';
 import 'features/home/data/repo/product_repo.dart';
 import 'features/splash_screen/view/logic/splash_cubit.dart';
@@ -28,8 +29,14 @@ void main() async {
 
   // Init Hive
   await Hive.initFlutter(); // to ask system do you give me space to put my data
-  Hive.registerAdapter(ProductModelAdapter());
-  Hive.registerAdapter(DetailsModelAdapter()); // Hive now under stand detail model
+  Hive.registerAdapter(ProductModelAdapter());// TypeId 0
+  Hive.registerAdapter(DetailsModelAdapter());// TypeId 1 // Hive now under stand detail model
+  Hive.registerAdapter(GetCartModelAdapter());// TypeId 2
+  Hive.registerAdapter(CartDataAdapter());// TypeId 3
+  Hive.registerAdapter(CartItemModelAdapter());// TypeId 4
+  Hive.registerAdapter(ToppingsAdapter());// TypeId 5
+  Hive.registerAdapter(SideOptionsAdapter());// TypeId 6
+
   await Hive.openBox<ProductModel>('productsBox');
   await Hive.openBox<DetailsModel>('toppingsBox'); // Initialization box when open app
   await Hive.openBox("userBox");// open the box fast to get token and image
