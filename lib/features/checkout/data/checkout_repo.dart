6e8dbd584ccbe80681db7404a0d3177a8cf54740
Mahmoud Yasser <mahmoudyaser20196@gmail.com>
2026-01-25 22:12:46 +0200

    finish checkOut cubit

--- lib/features/checkout/data/checkout_repo.dart
+++ lib/features/checkout/data/checkout_repo.dart
@@ -1,17 +1,18 @@
 
 
  import 'package:cheesegang/core/network/api_service.dart';
+import 'package:cheesegang/features/checkout/data/checkout_model.dart';
 import 'package:dio/dio.dart';
 
 import '../../../core/network/api_error.dart';
 import '../../../core/network/api_exception.dart';
-import '../../product/data/details_model.dart';
 
 class CheckoutRepo {
     ApiService apiService;
     CheckoutRepo({required this.apiService});
-     //Check out
-     Future<void> checkOut(CartRequestModel orderData )async{
+
+    //Check out
+     Future<void> checkOut(CheckoutModel orderData )async{
          try{
              final cartRequest = await apiService.post("/orders", orderData.toJson());
              if(cartRequest is ApiError){
--- lib/features/checkout/views/logic/checkout_cubit.dart
+++ lib/features/checkout/views/logic/checkout_cubit.dart
@@ -1,21 +1,19 @@
 
-/*
+
  import 'package:cheesegang/features/auth/data/auth_repo.dart';
 import 'package:cheesegang/features/auth/data/user_model.dart';
-import 'package:cheesegang/features/auth/view/profile_screen/logic/profile_state.dart';
 import 'package:cheesegang/features/checkout/data/checkout_model.dart';
 import 'package:cheesegang/features/checkout/data/checkout_repo.dart';
 import 'package:cheesegang/features/checkout/views/logic/checkout_state.dart';
-import 'package:cheesegang/features/orderHistory/data/order_model.dart';
-import 'package:cheesegang/features/orderHistory/data/order_repo.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/network/api_error.dart';
 
 class CheckoutCubit extends Cubit<CheckoutState>{
   final AuthRepo authRepo;
-  final OrderRepo orderRepo;
-  CheckoutCubit(this.authRepo, this.orderRepo):super(CheckoutInitial());
+  final CheckoutRepo checkOutRepo;
+  CheckoutCubit(this.authRepo, this.checkOutRepo):super(CheckoutInitial());
 
   String selectPayment = "cash";
   UserModel ? userModel;
@@ -29,22 +27,23 @@ class CheckoutCubit extends Cubit<CheckoutState>{
        emit(GetProfileLoading());
        try{
           final profileData = await authRepo.getProfileData();
-          emit(GetProfileSuccess(profileData!));
+          emit(GetProfileSuccess(user: profileData!));
 
        }catch(e){
          String msg = "Failed to get data";
          if(e is ApiError){
             msg = e.toString();
          }
-         emit(GetProfileError(msg));
+         emit(GetProfileError(message: msg));
        }
    }
 
-   Future<void> confirmOrder(OrderRequestModel orderData)async{
+   Future<void> confirmOrder(CheckoutModel orderData)async{
       emit(CheckoutLoading());
       try{
-        await orderRepo.saveOrder(orderData);
-        emit(CheckoutSuccess());
+      await checkOutRepo.checkOut(orderData);
+         emit(CheckoutSuccess(checkoutModel:orderData));
+
 
       }catch(e){
         String msg = "Failed to confirm order";
@@ -58,4 +57,3 @@ class CheckoutCubit extends Cubit<CheckoutState>{
 
 }
 
- */
