8b4c881cce0f59f71b79ccdd9870fcea6e8c22db
Mahmoud Yasser <mahmoudyaser20196@gmail.com>
2026-01-26 03:02:35 +0200

    finish orderHis state

---
+++ lib/features/orderHistory/views/logic/order_state.dart
@@ -0,0 +1,36 @@
+
+
+import 'package:cheesegang/features/cart/data/cart_model.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../data/order_model.dart';
+
+class OrderState extends Equatable{
+  @override
+  List<Object?> get props => [];
+
+}
+
+class OrderInitial extends OrderState{}
+class OrderLoading extends OrderState{
+  final OrderHisModel? orderHisModel;
+  OrderLoading({ this.orderHisModel});
+  @override
+  List<Object?> get props => [orderHisModel];
+}
+class OrderSuccess extends OrderState {
+  final OrderHisModel? orderHisModel;
+  OrderSuccess({required this.orderHisModel});
+  @override
+  List<Object?> get props => [orderHisModel];
+}
+
+class OrderError extends OrderState{
+  final String? message;
+  OrderError({this.message});
+}
+
+//gust mode
+class OrderGuest extends OrderState{}
+
+
