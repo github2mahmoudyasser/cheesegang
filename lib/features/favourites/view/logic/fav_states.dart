b4190681ced2c9a5bf75bef5126bcb6d0d8cfb74
Mahmoud Yasser <mahmoudyaser20196@gmail.com>
2026-01-27 17:02:17 +0200

    finish favourites states

--- lib/features/favourites/view/logic/fav_states.dart
+++ lib/features/favourites/view/logic/fav_states.dart
@@ -1,2 +1,27 @@
 
 
+     import 'package:cheesegang/features/favourites/data/favModel.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class FavStates extends Equatable {
+       @override
+       List<Object?> get props => [];
+     }
+
+     class FavInitial extends FavStates{}
+     class FavLoading extends FavStates{}
+
+     class FavSuccess extends FavStates{
+       final List<FavouritesModel> favourites;
+       FavSuccess({required this.favourites, });
+
+       @override
+       List<Object?> get props=> [favourites];
+     }
+
+     class FavFailure extends FavStates{
+       final String message;
+       FavFailure({required this.message});
+       @override
+       List<Object?> get props => [message];
+     }
